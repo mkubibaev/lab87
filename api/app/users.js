53166d12bef4c94a6f14b9f5319da45115d51ff5
mkubibaev <mkubibaev@gmail.com>
2019-04-25 18:09:32 +0600

    user register/login

--- api/app/users.js
+++ api/app/users.js
@@ -4,14 +4,36 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const users = await User.find();
-        res.send(users);
-    } catch {
-        res.sendStatus(500);
+        const user = new User(req.body);
+
+        user.generateToken();
+        await user.save();
+
+        return res.send({message: 'User registered', user});
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.post('/sessions', async (req, res) => {
+    const user = await User.findOne({login: req.body.login});
+
+    if (!user) {
+        return res.status(400).send({message: 'username or password incorrect'});
     }
 
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+        return res.status(400).send({message: 'username or password incorrect'});
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send({message: 'Login successful', user});
 });
 
 module.exports = router;
